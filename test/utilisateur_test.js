32004db753c738981de9c9bd504cd887cbcd3c3f
Yahia <yroyro59@gmail.com>
2015-12-04 23:44:58 +0100

    Ajout des test d'affichges des differents div pour la connexion utilisateur

--- test/utilisateur_test.js
+++ test/utilisateur_test.js
@@ -32,3 +32,47 @@ test("test getter utilisateur", function()
 	equal(u.getPseudo(),"pseudoTest","nouvel utilisateur avec le pseudo pseudoTest");
 }
 );
+
+test("test suppression menuConnexion, divLogin et divAccueil apres connection de l'utilisateur",3, function()
+{
+	var fixture="";
+        fixture+=("<div id='principal'>");
+        fixture+=("<ul id='menuConnexion'></ul>");
+        fixture+=("<div id='divLogin'></div>");
+        fixture+=("<div id='divAccount'></div>");
+        fixture+=("<div id='divAccueil'></div>");
+        fixture+=("</div>");
+
+	var u= new Utilisateur();
+	var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+	u.connect("pseudoTest");
+	var menuConnexion = document.getElementById("menuConnexion");
+	var divLogin = document.getElementById("divLogin");
+	var divAccount = document.getElementById("divAccount");
+	equal(menuConnexion,null,"le menuConnexion est supprimee");
+	equal(divLogin,null,"le divLogin est supprimee");
+	equal(divAccount,null,"le divAccueil est supprimee");
+}
+);
+
+test("test affichage divAccueil apres connexion de l'utilisateur", function()
+{
+	var fixture="";
+        fixture+=("<div id='principal'>");
+        fixture+=("<ul id='menuConnexion'></ul>");
+        fixture+=("<div id='divLogin'></div>");
+        fixture+=("<div id='divAccount'></div>");
+        fixture+=("<div id='divAccueil'></div>");
+        fixture+=("</div>");
+
+	var u= new Utilisateur();
+	var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+	u.connect("pseudoTest");
+	var divAccueil = document.getElementById("divAccueil");
+	var styleDivAcceuil = divAccueil.style.display;
+	equal(styleDivAcceuil,'block',"le divAccueil est supprimee");
+}
+);
+
