aea314ff42d2218fe958a4b750028d6ddea7e72d
Yahia <yroyro59@gmail.com>
2015-12-07 10:41:17 +0100

    Ajout des tests de connection

---
+++ test/ConnectionTest.js
@@ -0,0 +1,22 @@
+module("connection", {
+//	setup:function(){alert("setup money individual test");},
+//	teardown:function(){alert("teardown money individual test");}
+});
+
+test("test getter pseudo connection",function()
+{
+	var pseudo = "mohcine";
+	var mdp = "monmdp";
+	var c=new Connection(pseudo,mdp);
+	equal(c.pseudo,pseudo,"test du getter pseudo");
+}
+);
+
+test("test getter mdp connection",function()
+{
+	var pseudo = "mohcine";
+	var mdp = "monmdp";
+	var c=new Connection(pseudo,mdp);
+	equal(c.mdp,mdp,"test du getter pseudo");
+}
+);
